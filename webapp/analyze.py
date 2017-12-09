9677b781bf2afa319fd5622b77f74c1bff60ae5e
Tomoto <tomoto@gmail.com>
2017-12-09 16:09:38 +0900

    Added applicationcode.

---
+++ webapp/analyze.py
@@ -0,0 +1,114 @@
+from __future__ import division
+import sys
+import logging
+import cv2
+import numpy as np
+import colorsys
+# import matplotlib
+# import matplotlib.pyplot as plt
+
+class Level:
+
+    def __new__(cls):
+        _value_ = "level"
+
+    def __init__(self, level, hue, required_xp, next):
+        self.level = level
+        self.hue = hue
+        self.required_xp = required_xp
+        self.next = next
+
+Level.GOLD = Level(3, 26, 30000, None)
+Level.SILVER = Level(2, 98, 4000, Level.GOLD)
+Level.BRONZE = Level(1, 14, 500, Level.SILVER)
+Level.NONE = Level(0, 55, 0, Level.BRONZE)
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Screen metrics at width 1080
+SCREEN_WIDTH = 1080
+FULL_BAR_WIDTH = 136
+
+def dimension(img):
+    return (len(img[0]), len(img))
+
+def hsv2bgr(h, s, v):
+    # too complicated, I hate this
+    return (cv2.cvtColor(np.array([[[h, s, v]]], np.uint8), cv2.COLOR_HSV2BGR)[0][0]).astype(np.int)
+
+def compare(a, b, eps):
+    return abs(a - b) <= eps
+
+def calc_remaining_xp(bar_width, badge_level):
+    return max((1.0 - bar_width / FULL_BAR_WIDTH) * badge_level.next.required_xp, 0)
+
+def determine_badge_level(hsv_img, bar_rect):
+    delta_x, delta_y = (88, -6)
+    hsv = hsv_img[bar_rect[1] + delta_y][bar_rect[0] + delta_x]
+    logging.debug("Badge color: %s", hsv)
+    for level in (Level.SILVER, Level.BRONZE, Level.NONE):
+        if compare(hsv[0], level.hue, 3):
+            return level
+    return None
+
+def validate_bar_position(bar_rect):
+    if bar_rect[0] < SCREEN_WIDTH - FULL_BAR_WIDTH:
+        for possible_bar_x in range(115, SCREEN_WIDTH - FULL_BAR_WIDTH, 345):
+            if compare(bar_rect[0], possible_bar_x, 3):
+                return True
+    return False
+
+def validate_bar_size(bar_rect):
+    return compare(bar_rect[3], 10, 3)
+
+def find_bar_rects(bar_img):
+    _, contours, hierarcy = cv2.findContours(bar_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return [cv2.boundingRect(c) for c in contours]
+
+def extract_bar_img(hsv_img):
+    lower_color = np.array([80, 180, 0])
+    upper_color = np.array([90, 255, 255])
+    return cv2.inRange(hsv_img, lower_color, upper_color)
+
+def draw_xp_text(img, rect, xp, fg_color, bg_color):
+    font_face = cv2.FONT_HERSHEY_DUPLEX
+    font_size = 1.25
+    font_thickness = 3
+    text = str(int(xp))
+    text_size = cv2.getTextSize(text, font_face, font_size, font_thickness)
+    text_offset = 5
+    text_margin = 5
+    cv2.rectangle(img,
+                  (rect[0], rect[1] - text_offset - text_size[0][1] - text_margin * 2),
+                  (rect[0] + FULL_BAR_WIDTH, rect[1] - text_offset),
+                  bg_color, -1)
+    cv2.putText(img, text,
+                (rect[0] + FULL_BAR_WIDTH - text_size[0][0], rect[1] - text_offset - text_margin),
+                font_face, font_size, fg_color, font_thickness, cv2.LINE_AA)
+
+def process_image_data(data, extension):
+    npdata = np.fromstring(data, np.uint8)
+    orig_img = cv2.imdecode(npdata, cv2.IMREAD_COLOR)
+    hsv_img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2HSV)
+
+    bar_img = extract_bar_img(hsv_img)
+
+    bar_rects = find_bar_rects(bar_img)
+
+    result_img = np.copy(orig_img)
+
+    for rect in bar_rects:
+        logging.debug("Bounding rect: %s", rect)
+        if not (validate_bar_size(rect) and validate_bar_position(rect)):
+            continue
+        badge_level = determine_badge_level(hsv_img, rect)
+        logging.debug("Badge level: %s", badge_level)
+        if badge_level == None:
+            continue
+        remaining_xp = calc_remaining_xp(rect[2], badge_level)
+        logging.debug("Remaining XP: %s", remaining_xp)
+        bg_color = hsv2bgr(badge_level.next.hue, 120, 240)
+        draw_xp_text(result_img, rect, remaining_xp, (0, 0, 0), bg_color)
+
+    result, buf = cv2.imencode(extension, result_img)
+    return buf.tobytes()
--- webapp/app.py
+++ webapp/app.py
@@ -1,13 +1,31 @@
 import os
-import cv2
+import io
+from analyze import process_image_data
+from flask import Flask, render_template, redirect, request, send_file
 
-from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return cv2.__version__
+def index():
+    return redirect("upload_image")
+
+@app.route('/upload_image', methods=['GET', 'POST'])
+def upload_image():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No uploaded file')
+            redirect(request.url)
+        f = request.files['file']
+        if f.filename == '':
+            flash('No uploaded file')
+            redirect(request.url)
+        result_image_data = process_image_data(f.read(), ".png")
+        return send_file(io.BytesIO(result_image_data),
+                         attachment_filename='gymranks.png',
+                         mimetype='image/png')
+
+    return render_template("upload_image.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
